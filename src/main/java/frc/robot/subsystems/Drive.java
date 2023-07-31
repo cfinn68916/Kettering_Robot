ef486ae8e4d6d8cbc315a2b6170c92a67daa76ec
Colin <cfinn68916@gmail.com>
2023-07-31 11:37:13 -0400

    Code meets robot
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,70 +1,41 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
-  CANSparkMax frontLeft = new CANSparkMax(Constants.IO.FRONT_LEFT_ID, MotorType.kBrushless);
-  CANSparkMax rearLeft = new CANSparkMax(Constants.IO.BACK_LEFT_ID, MotorType.kBrushless);
-  CANSparkMax frontRight = new CANSparkMax(Constants.IO.FRONT_RIGHT_ID, MotorType.kBrushless);
-  CANSparkMax rearRight = new CANSparkMax(Constants.IO.BACK_RIGHT_ID, MotorType.kBrushless);
-
-  RelativeEncoder leftEncoder = frontLeft.getEncoder();
-  RelativeEncoder rightEncoder = frontRight.getEncoder();
+  PWMVictorSPX frontLeft = new PWMVictorSPX(Constants.IO.FRONT_LEFT_ID);
+  PWMVictorSPX rearLeft = new PWMVictorSPX(Constants.IO.BACK_LEFT_ID);
+  PWMVictorSPX frontRight = new PWMVictorSPX(Constants.IO.FRONT_RIGHT_ID);
+  PWMVictorSPX rearRight = new PWMVictorSPX(Constants.IO.BACK_RIGHT_ID);
 
   MotorControllerGroup left = new MotorControllerGroup(frontLeft, rearLeft);
   MotorControllerGroup right = new MotorControllerGroup(frontRight, rearRight);
 
+  public void setVolts(double leftV, double rightV) {
+    left.setVoltage(leftV);
+    right.setVoltage(rightV);
+  }
+
   DifferentialDrive drive = new DifferentialDrive(left, right);
   ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-  DifferentialDriveOdometry odometry;
 
   public Drive() {
     right.setInverted(true);
-    odometry = new DifferentialDriveOdometry(getGyroYaw(), getLeftEncoder(), getRightEncoder());
   }
 
-  public void periodic() {
-    odometry.update(getGyroYaw(), getLeftEncoder(), getRightEncoder());
-  }
+  public void periodic() {}
 
   public Rotation2d getGyroYaw() {
     return gyro.getRotation2d();
   }
 
-  public Pose2d getPose() {
-    return odometry.getPoseMeters();
-  }
-
   public void humanDrive(double fwd, double rot) {
     drive.arcadeDrive(fwd, rot, false);
   }
-
-  public double getLeftEncoder() {
-    return leftEncoder.getPosition() * Constants.Drivetrain.EFFECTIVE_WHEEL_DIAMETER;
-  }
-
-  public double getRightEncoder() {
-    return rightEncoder.getPosition() * Constants.Drivetrain.EFFECTIVE_WHEEL_DIAMETER;
-  }
-
-  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-    return new DifferentialDriveWheelSpeeds(getLeftEncoder(), getRightEncoder());
-  }
-
-  public void setVolts(double leftV, double rightV){
-    left.setVoltage(leftV);
-    right.setVoltage(rightV); 
-  }
-
 }
